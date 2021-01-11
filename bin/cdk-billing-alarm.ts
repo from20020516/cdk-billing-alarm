9065cb69de6c0f342c16134d2472efd8c2779524
from20020516 <from20020516@gmail.com>
2021-01-12 03:13:26 +0900

    Change aws region to globally us-east-1.

--- bin/cdk-billing-alarm.ts
+++ bin/cdk-billing-alarm.ts
@@ -4,4 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { CdkBillingAlarmStack } from '../lib/cdk-billing-alarm-stack';
 
 const app = new cdk.App();
-new CdkBillingAlarmStack(app, 'CdkBillingAlarmStack');
+new CdkBillingAlarmStack(app, 'CdkBillingAlarmStack', {
+    env: {
+        region: 'us-east-1'
+    },
+});
