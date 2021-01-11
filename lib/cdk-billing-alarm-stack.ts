a5be31b84b006dde8d55f33e5be1be05c364feaf
from20020516 <from20020516@gmail.com>
2021-01-11 16:02:28 +0900

    Add Billing Alarm. usage: cdk deploy --context email=$YOUR_EMAIL_ADDRESS

--- lib/cdk-billing-alarm-stack.ts
+++ lib/cdk-billing-alarm-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch'
+import * as cloudwatchAlarm from '@aws-cdk/aws-cloudwatch-actions'
+import * as sns from '@aws-cdk/aws-sns'
+import * as snsSubscriptions from '@aws-cdk/aws-sns-subscriptions'
 
 export class CdkBillingAlarmStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const billingTopic = new sns.Topic(this, 'BillingTopic')
+    billingTopic.addSubscription(new snsSubscriptions.EmailSubscription(this.node.tryGetContext('email')))
+
+    new cloudwatch.Alarm(this, 'BillingAlarm', {
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      evaluationPeriods: 1,
+      metric: new cloudwatch.Metric({
+        metricName: 'EstimatedCharges',
+        namespace: 'AWS/Billing',
+        statistic: 'Maximum',
+        dimensions: {
+          Currency: 'JPY',
+        },
+      }).with({ period: cdk.Duration.hours(6) }),
+      threshold: this.node.tryGetContext('threshold') ?? 5000
+    }).addAlarmAction(new cloudwatchAlarm.SnsAction(billingTopic))
   }
 }
