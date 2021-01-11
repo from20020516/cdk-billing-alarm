c25a466aaf5e65404481743cd25d51a9f246f4f6
from20020516 <from20020516@gmail.com>
2021-01-11 15:59:06 +0900

    Initial commit

---
+++ bin/cdk-billing-alarm.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkBillingAlarmStack } from '../lib/cdk-billing-alarm-stack';
+
+const app = new cdk.App();
+new CdkBillingAlarmStack(app, 'CdkBillingAlarmStack');
